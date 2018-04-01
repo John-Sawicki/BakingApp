a040e57919b354ef73a7f4c8b715f1abbbcd0431
john-sawicki <3Gxapnil>
2018-04-01 18:36:53 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/bakingapp/Utilities/RecipeAdapter.java
@@ -0,0 +1,4 @@
+package com.example.android.bakingapp.Utilities;
+
+public class RecipeAdapter {
+}
---
+++ app/src/main/java/com/example/android/bakingapp/Utilities/StepAdapter.java
@@ -0,0 +1,23 @@
+package com.example.android.bakingapp.Utilities;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+public class StepAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position, List payloads) {
+        super.onBindViewHolder(holder, position, payloads);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
