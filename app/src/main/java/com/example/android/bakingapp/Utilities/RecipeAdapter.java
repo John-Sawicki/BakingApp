8a02b0f33465cfbb590398ee7fcd615c0cf1fecf
john-sawicki <3Gxapnil>
2018-06-17 21:36:25 -0500

    added user permissions

--- app/src/main/java/com/example/android/bakingapp/Utilities/RecipeAdapter.java
+++ app/src/main/java/com/example/android/bakingapp/Utilities/RecipeAdapter.java
@@ -1,4 +1,36 @@
 package com.example.android.bakingapp.Utilities;
 
-public class RecipeAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeAdapterVH> {
+    String[] mRecipes = {"cake","cookies", "pie" };
+    public RecipeAdapter(){
+
+    }
+    public class RecipeAdapterVH extends RecyclerView.ViewHolder{
+        private final TextView mRecipeTV;
+        private RecipeAdapterVH(View view){
+            super(view);
+            mRecipeTV = view.findViewById(R.id.recipe_text_view);
+        }
+    }
+    @Override
+    public RecipeAdapterVH onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecipeAdapterVH holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
--- app/src/main/java/com/example/android/bakingapp/Utilities/StepAdapter.java
+++ app/src/main/java/com/example/android/bakingapp/Utilities/StepAdapter.java
@@ -1,18 +1,36 @@
 package com.example.android.bakingapp.Utilities;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.List;
 
-public class StepAdapter extends RecyclerView.Adapter {
+public class StepAdapter extends RecyclerView.Adapter<StepAdapter.StepAdapterVH> {
+    private String[] steps = {"preheat oven", "mix ingrediants", "shale and bake"};
+    public StepAdapter(){
+
+    }
+    public class StepAdapterVH extends RecyclerView.ViewHolder{
+        private final TextView mRecipeTV;
+        private StepAdapterVH(View view){
+            super(view);
+            mRecipeTV = view.findViewById(R.id.recipe_text_view);
+        }
+    }
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public StepAdapter.StepAdapterVH onCreateViewHolder(ViewGroup parent, int viewType) {
         return null;
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position, List payloads) {
+    public void onBindViewHolder(StepAdapterVH holder, int position) {
+
+    }
+
+    @Override
+    public void onBindViewHolder(StepAdapterVH holder, int position, List<Object> payloads) {
         super.onBindViewHolder(holder, position, payloads);
     }
 
@@ -21,3 +39,4 @@ public class StepAdapter extends RecyclerView.Adapter {
         return 0;
     }
 }
+
