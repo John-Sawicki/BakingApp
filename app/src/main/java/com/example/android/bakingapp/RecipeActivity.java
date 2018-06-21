02de229ec77735f84099c756dfd293f0381109d9
john-sawicki <3Gxapnil>
2018-06-20 22:33:14 -0500

    added butterknife

--- app/src/main/java/com/example/android/bakingapp/RecipeActivity.java
+++ app/src/main/java/com/example/android/bakingapp/RecipeActivity.java
@@ -3,15 +3,22 @@ package com.example.android.bakingapp;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.android.bakingapp.Utilities.JsonUtility;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class RecipeActivity extends AppCompatActivity {
     private static String JsonUrl = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
+    @BindView(R.id.recycler_view_phone) RecyclerView mRecyclerViewPhone;
+    @BindView(R.id.recycler_view_tablet)RecyclerView mRecyclerViewTablet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+        ButterKnife.bind(this);
     }
     public class GetRecipes extends AsyncTask<Void, Void, String[]>{
         String[] recepeNames = new String[13];
