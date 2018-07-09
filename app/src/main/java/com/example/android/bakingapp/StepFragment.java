2700dd4bf259b5893264bf0011be501916234c58
john-sawicki <3Gxapnil>
2018-07-09 11:19:44 -0500

    displays no video and loading video and instructions text

--- app/src/main/java/com/example/android/bakingapp/StepFragment.java
+++ app/src/main/java/com/example/android/bakingapp/StepFragment.java
@@ -1,6 +1,5 @@
 package com.example.android.bakingapp;
 
-
 import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -11,7 +10,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.android.bakingapp.Utilities.JsonUtility;
@@ -26,7 +24,6 @@ import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
@@ -35,9 +32,6 @@ public class StepFragment extends Fragment {
     @BindView(R.id.step_movie)SimpleExoPlayerView step_movie;
     @BindView(R.id.no_video_text)TextView no_video_text;
     @BindView(R.id.instruction_text)TextView instruction_text;
-    //SimpleExoPlayer step_movie;
-   // ImageView no_video_image;
-   // TextView instruction_text;
     Context mContext;
     SimpleExoPlayer mExoPlayer;
     private Unbinder mUnbinder;
@@ -54,7 +48,9 @@ public class StepFragment extends Fragment {
         mContext= container.getContext();
         mUnbinder= ButterKnife.bind(this,view);
         no_video_text.setVisibility(View.VISIBLE);
+        no_video_text.setText(R.string.check_for_video);
         step_movie.setVisibility(View.INVISIBLE);
+        instruction_text.setText(R.string.loading_message);
         Bundle bundle = getArguments();
         stepFragRecipeStepValues[0] = bundle.getInt("recipeIndex",0);
         Log.d("stepFragment onC rec",stepFragRecipeStepValues[0]+"");
@@ -63,16 +59,8 @@ public class StepFragment extends Fragment {
         new getInstructionsLong().execute(stepFragRecipeStepValues);    //first element is the long description, second is the movie url
         return view;
     }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        //TODO turn off exoplayer
-    }
-
     public class getInstructionsLong extends AsyncTask<int[], Void, String[]> {
         String StepLongMovieUrl[] = new String[2];  //first element is the long description, second is the movie url
-
         @Override
         protected String[] doInBackground(int[]... integers) {
             try{
@@ -96,13 +84,14 @@ public class StepFragment extends Fragment {
                 instruction_text.setText(strings[0]);
             }else{
                 Log.d("stepFragment noText", "no instructions");
-                instruction_text.setText("There are no instructions for this step.");
+                instruction_text.setText(R.string.no_instruction_text);
             }
             if(strings[1]!=null)Log.d("stepFragment Post 1  ",strings[1] );
 
             if(strings[1]==null || strings[1]==""||strings[1]=="empty"|| strings[1].length()==0){    //valid urls start with http
                 Log.d("stepFragment noVideo", "no video");
                 no_video_text.setVisibility(View.VISIBLE);
+                no_video_text.setText(R.string.no_video_text);
                 step_movie.setVisibility(View.INVISIBLE);
             }else{
                 Log.d("stepFragment Video", strings[1]);
@@ -124,30 +113,6 @@ public class StepFragment extends Fragment {
                     Log.e("stepFragment catch", " exoplayer error " + e.toString());
                 }
             }
-            /*
-            if(strings[1]!=null && strings[1]!=""){
-                Log.d("stepFragment Video", strings[1]);
-                no_video_text.setVisibility(View.INVISIBLE);
-                step_movie.setVisibility(View.VISIBLE);
-                try {
-                    BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
-                    TrackSelector trackSelector = new DefaultTrackSelector(new AdaptiveTrackSelection.Factory(bandwidthMeter));
-                    mExoPlayer = ExoPlayerFactory.newSimpleInstance(mContext, trackSelector);
-                    //Uri videoURI = Uri.parse(testUrl);
-                    Uri videoURI = Uri.parse(strings[1]);
-                    DefaultHttpDataSourceFactory dataSourceFactory = new DefaultHttpDataSourceFactory("exoplayer_video");
-                    MediaSource mediaSource = new ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(videoURI);
-                    step_movie.setPlayer(mExoPlayer);
-                    mExoPlayer.prepare(mediaSource);
-
-                    mExoPlayer.setPlayWhenReady(true);
-                }catch (Exception e){
-                    Log.e("stepFragment catch", " exoplayer error " + e.toString());
-                }
-            }else{
-
-            }
-            */
         }
     }
     public void setDescAndURL(int[] intDescAndUrl){
